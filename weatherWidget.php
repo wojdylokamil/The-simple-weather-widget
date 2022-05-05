4ec320d8ff246570760fdeb276e624bd3d2da772
WojdyloKamil <wojdylokamil.dev@gmail.com>
2022-05-05 19:20:52 +0200

    Improving functionality

--- weatherWidget.php
+++ weatherWidget.php
@@ -2,29 +2,33 @@
 
 $ch = curl_init();
 
-      curl_setopt($ch, CURLOPT_URL, "https://danepubliczne.imgw.pl/api/data/synop");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      $cities = json_decode(curl_exec($ch), true);
-      curl_close($ch);
+curl_setopt($ch, CURLOPT_URL, "https://danepubliczne.imgw.pl/api/data/synop");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$cities = json_decode(curl_exec($ch), true);
+curl_close($ch);
 
 $citiesNames = array();
-$pickedCity = '';
+$error = '';
 
-foreach($cities as $city){
+
+foreach ($cities as $city) {
       $cityName = $city['stacja'];
       $citiesNames[] = $cityName;
+      $citiesNames[] = strtolower($cityName);
+      if ($city['stacja'] = 'Warszawa') {
+            $pickedCity = $city;
+      };
 };
 
-if(isset($_POST['searchingCity']) && $_POST['searchingCity'] !== ''){
+if (isset($_POST['changeCity']) && $_POST['searchingCity'] !== '') {
       $searchingCity = $_POST['searchingCity'];
       foreach ($cities as $city) {
-            if ($city['stacja'] == $searchingCity) {
-            $pickedCity = $city;
+            if ($searchingCity == $city['stacja'] || $searchingCity == strtolower($city['stacja'])) {
+                  $pickedCity = $city;
             };
       };
       if (!in_array($searchingCity, $citiesNames)) {
-            $searchingCity = '';
-            header("Refresh:0");
+            $city = '';
+            $error = "City not founded";
       };
 };
-
